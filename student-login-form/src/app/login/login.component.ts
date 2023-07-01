36bb9b81d90a8302b7736fe95537eeeac52e8737
saisatyateja <saisatyatejagunisetti@gmail.com>
2023-07-01 13:05:14 +0530

    angular first commit

---
+++ student-login-form/src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = "";
+  password: string = "";
+  loggedIn: boolean = false;
+
+  submitForm() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+    if (this.username === 'siri' && this.password === 'srividya_1418') {
+      this.loggedIn = true;
+      console.log('Login successful');
+      window.location.href = 'https://qualitythought.in';
+      // Redirect to a different page or perform any other necessary actions upon successful login
+    } else {
+      console.log('Invalid username or password');
+      // Display an error message or perform any other necessary actions upon unsuccessful login
+    }
+  }
+}
